76aa4ba2f01d49b5cb8ceff944870534b0aec8cf
naveed <786muhammadnaveed@gmail.com>
2022-03-11 18:31:38 +0500

    Assesment

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -14,7 +14,7 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12 text-center mb-3">
-                <h1>Assessment - Metaschool => Naveed _ Web Developer _ 3+</h1>
+                <h1>Assessment - Metaschool => Naveed _ Web Developer _  3+</h1>
             </div>
             <div class="col-lg-4">
                 <div class="card">
